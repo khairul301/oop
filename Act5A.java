b444effc3fb969a462ded5322ecff19b8bfe172f
khairul301 <khairul301@gmail.com>
2015-08-10 13:02:08 +0800

    Update and rename README.md to Act5A.java

---
+++ Act5A.java
@@ -0,0 +1,32 @@
+/*Khairul Anuar*/
+
+class Act5A  
+{ 
+public static void main(String[] args)  
+{ 
+      int testscore = 76; 
+      char grade; 
+      if (testscore
+      >
+      = 80)  
+            grade = 'A'; 
+      else  
+      if (testscore
+      >
+      = 70)  
+            grade = 'B'; 
+      else  
+      if (testscore
+      >
+      = 60)  
+            grade = 'C'; 
+      else  
+      if (testscore
+      >
+      = 50)  
+            grade = 'D'; 
+      else 
+            grade = 'F'; 
+      System.out.println ("Grade = " + grade); 
+  } 
+} 
